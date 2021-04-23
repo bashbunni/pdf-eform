4701be1db6cac305bf06d1e21c19678b9f583084
bashbunni <bashbunni.io@gmail.com>
2021-04-23 11:16:34 -0700

    broken openCV commit :D

--- main.py
+++ main.py
@@ -17,19 +17,39 @@
 #
 
 import sys
+import cv2
+import numpy
+import pdf2image
 # image width should be 1500px
 # may be multiple pages
 # def get_ffields():
 
+def detect_lines():
+    '''
+    @param img string of image path
+    @return coordinates?
+    '''
+    img = "adolescentinpatientunitInterhospital-Transfer-form-1.png"
+    myimg = cv2.imread(img) 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize = 3)
+    min_line_length = 100
+    max_line_gap = 10
+    lines = cv2.HoughLinesP(edges, 1, numpy.pi/180, 100, min_line_length, max_line_gap)
+    for x1, y1, x2, y2 in lines[0]:
+        cv2.line(img, (x1, y1), (x2,y2), (0, 255, 0), 2)
+    cv2.imwrite('houghlines-test1.png', img)
+
 
 if __name__ == "__main__":
-    print("running")
-    print(len(sys.argv))
+   # print("running")
+    # print(len(sys.argv))
     # want to be like python3 pdftohtml.py path/to/mypdf.pdf 2
-    if len(sys.argv) <= 1: # additional params were given
-        sys.exit("Please provide a path to the PDF and the number of pages")
-    else:
-        pdf2image(sys.argv[1], size=(1500, None)) # get PDF file path
+    # if len(sys.argv) <= 1: # additional params were given
+    #    sys.exit("Please provide a path to the PDF and the number of pages")
+    # else:
+    #    pdf2image(sys.argv[1], size=(1500, None)) # get PDF file path
         # optional number of pages 
-    print("number of pages: " + len(sys.argv)-1)
+    # print("number of pages: " + len(sys.argv)-1)
 #    images = convert_from_path('/home/belval/example.pdf')
+    detect_lines()
